cec82c78d0aa849ec187bafce1868dce96869739
Aditya <adityaboyanapalli09@gmail.com>
2026-08-27 20:22:59 +0530

    ✅ For Loop [cpp] — approach1

---
+++ solutions/cpp/easy/c-tutorial-for-loop/solution-approach1.cpp
@@ -0,0 +1,60 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem?isFullScreen=true
+// Problem     For Loop
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-27, 08:22 p.m.
+// ──────────────────────────────────────────────────
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    
+    cin>> a;
+    cin>>b;
+    
+    for (int n =a; n<=b; n++){
+        
+        if(n==1){
+            cout << "one" << '\n';
+        }
+        else if (n==2){
+            cout << "two" << '\n';
+        }
+        else if (n==3){
+            cout << "three" << '\n';
+        }
+        else if (n==4){
+            cout << "four" << '\n';
+        }
+        else if (n==5){
+            cout << "five" << '\n';
+        }
+        else if (n==6){
+            cout << "six" << '\n';
+        }
+        else if (n==7){
+            cout << "seven" << '\n';
+        }
+        else if (n==8){
+            cout << "eight" << '\n';
+        }
+        else if (n==9){
+            cout << "nine" << '\n';
+        }
+        else if (n%2==0){
+            cout << "even" << '\n';
+        }
+        else {
+            cout << "odd" << '\n';
+        }
+    }
+    return 0;
+}
