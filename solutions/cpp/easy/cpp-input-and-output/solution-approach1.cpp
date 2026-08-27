69a3591f37db95a6aba1ae0620b7bd66a29bed0c
Aditya <adityaboyanapalli09@gmail.com>
2026-08-27 20:22:07 +0530

    ✅ Input and Output [cpp] — approach1

---
+++ solutions/cpp/easy/cpp-input-and-output/solution-approach1.cpp
@@ -0,0 +1,32 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+// Problem     Input and Output
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-08-27, 08:21 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+    int a,b,c;
+    
+    cin >> a >> b >> c;
+    
+    int result = a + b +c;
+    
+    cout << result;
+
+
+    return 0;
+}
